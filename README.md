f2dde2afea4aff40ba6df63700d18e428f0d5a39
Kecvin-12-u <65631970+Kevin12-0@users.noreply.github.com>
2023-04-04 13:41:57 -0600

    Vercion 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# pwa_kevin
-pwa para derroollo con node.js
+#Crear una PWA Con Vue.js
+
+1. en terminal ejecutar
+
+```cmd
+    vue create nombre_app
+```
+
+2. Seleccionar Manuely config
+
+3. Seleccionar la opcion de PWA support (utilizando la tecla de espacio)
+
+4. en los siguientes pasos dar enter
+
+5. una ver terminado la descarga, ejecutar lo siguiente en terminal
+
+```cmd
+    cd nombre_proyecto
+    npm intall
+```
+
+6. terminada la intalación ejecutar lo siguiete, para que te genere los archivos necesarios para tu PWA
+
+```cmd
+    yarn build
+```
+
--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 9000
-const http = require('http');
-const server = http.createServer(app);
-
-
 const path = require('path');
+const router = express.Router();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/templates/index.html'))
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname,'/views/index.html'));
 });
 
-server.listen(PORT, () => console.log('listening on port'));
+app.use("/".router);
+app.listen(process.env.port || 3000);
+
+
