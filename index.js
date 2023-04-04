e9a7fe75b7d662be837da7157c17a8e3ed592a18
Kecvin-12-u <65631970+Kevin12-0@users.noreply.github.com>
2023-04-04 14:48:54 -0600

    Vertion 1.0.0

--- index.js
+++ index.js
@@ -7,9 +7,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/views/index.html'));
+    res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
+router.get("/app", function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/js/app.a5008d55.js'));
+});
+
+router.get("/chunk-vendors", function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/js/chunk-vendors.4d7ee3c4.js'));
+});
+
+router.get("/css", function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/css/app.0457deba.css'));
+});
+
+router.get("/manifest", function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/manifest.json'));
+});
 app.use("/", router);
 app.listen(process.env.port || 3000);
 
