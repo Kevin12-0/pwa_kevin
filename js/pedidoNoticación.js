49b9f456e3337b8e9b99ebe0f75fd4bd486f06a0
Kecvin-12-u <65631970+Kevin12-0@users.noreply.github.com>
2023-04-08 15:22:22 -0600

    vertion 1.3.0

--- js/pedidoNoticación.js
+++ js/pedidoNoticación.js
@@ -1,5 +1,16 @@
 const pedidoNotification = async () => {    //Vamos a comprobar si el navegador es compatible con las notificaciones
-    if (navigator.setAppBadge) {
-        console.log("The App Badging API is supported!");
+    if (Notification.permission !== "granted") {
+        Notification.requestPermission();
+    }
+
+    if (Notification.permission === "granted") {
+        const notification = new Notification("Novedades el Inge", {
+            body: "Tu pedido a sido realizado, espera tu entrega",
+            icon: "../img/icons/favicon.ico",
+        });
+
+        notification.addEventListener("click", (event) => {
+            // Acción a realizar cuando se hace clic en la notificación
+        });
     }
 }
